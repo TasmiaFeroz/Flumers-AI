c01b2675f5711f03b1fdfb634864d0804bc32708
Tasmia Feroz <tasmiaferoz7@gmail.com>
2025-09-19 12:26:51 +0500

    Update settings.py

--- backend/apis/apis/settings.py
+++ backend/apis/apis/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aiz0gvpaj81x_a5uhm$m!dxta$p)!)_&khpz*#83+usu$$-3c!'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
